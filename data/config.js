c30e8de65196ef053927dd21bc56aacf0c73d30c
farrelmr <farrelmr@users.noreply.github.com>
2019-04-03 21:25:39 +0100

    Update config.js

--- data/config.js
+++ data/config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Quality and Affordable Shopify Development for your Business.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
@@ -12,7 +12,7 @@ module.exports = {
   userName: 'Martin Farrell',
   userTwitter: 'vaibhaved',
   userLocation: 'Edinburgh, Scotland',
-  userDescription: '',
+  userDescription: 'Java, React and Shopify Developer',
   copyright: 'Copyright © Glendevon Software Ltd 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
